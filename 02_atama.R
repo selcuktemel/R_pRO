2b7116dca5789c2fe6fa931bf761d0980f13998d
tomdigital <tomdigital@testinium.io>
2023-12-09 19:24:29 +0300

    atamalar

---
+++ 02_atama.R
@@ -0,0 +1,11 @@
+a=5
+b=2
+a=16
+b=10
+# ilk başta dosyayı kaydetmemiz gerekiyor sonrasında çalıştırıdğımızda ve 
+# atama yaptığımızda 
+
+# üç tip atama yöntemi var
+# <- ,biz atamalarımızda bunu kullanacağız
+# =, 
+# <<- 
---
+++ 03_atama.R
@@ -0,0 +1,34 @@
+# çarpma,bölme, toplama cıkarma üstlü sayılarla işlemler yapma
+# Aritmetik ve Mantık Komutları
+
+# + - / * ^üslü sayı alma
+
+10+10 # anında hesap yapabiliriz R studio ile
+
+a <- 10+10
+b <- 303
+c <- a*b
+print(c)
+c <- b / c
+
+# Mantıksal Operatörler
+#  <
+# >
+# <=
+# >=
+# !=
+# ==
+# & ya da -veya or
+# | and ve komutu
+
+
+veri<- 1:20 # 1 ile 20 dahil arasında 
+
+veri [veri==5] # bu değişken içersinde 5 var mı>> 1 adet 5 olduğunu söylüyor [1] 5
+
+veri [veri>5] 
+veri [veri<5] 
+veri [veri!=5] # 5 dışındaki sayıları döner
+
+# 
+
